a97f50852f6b7e17112f625f6f80d15226a71caf
Yahia Hassan <77647892+YAHIA-HASSAN@users.noreply.github.com>
2024-05-20 00:51:46 +0300

    inverted index and TF/IDF

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from inverted_index import retrieve_top_document_names as InvI_retrieve
 from TF_IDF import retrieve_top_document_names as TFIDF_retrieve
 import os
 
+
 def preprocess():
     process(os.path.dirname(__file__) + '\\Dataset\\',
             os.path.dirname(__file__) + '\\Preprocessed\\')
+
 
 def term_document_matrix(query):
     top_doc_names = TDM_retrieve(query)
@@ -14,10 +16,12 @@ def term_document_matrix(query):
     for idx, doc_name in enumerate(top_doc_names):
         print(f"Top Document {idx + 1}: {doc_name}")
 
+
 def inverted_index(query):
     top_doc_names = InvI_retrieve(query)
     for idx, doc_name in enumerate(top_doc_names):
         print(f"Top Document {idx + 1}: {doc_name}")
+
 
 def TF_IDF(query):
     top_doc_names = TFIDF_retrieve(query)
@@ -25,20 +29,26 @@ def TF_IDF(query):
     for idx, doc_name in enumerate(top_doc_names):
         print(f"Top Document {idx + 1}: {doc_name}")
 
+
 if __name__ == '__main__':
     # preprocess()
-    ch = input("*Select Retrieving Algorithm*\n"
-               "1. Term Document Matrix\n"
-               "2. Inverted Index\n"
-               "3. TF/IDF\n")
-    query = input("\n---------------------\n"
-                  "Enter Query: ")
-    print("---------------------\n")
-    if ch == "1":
-        term_document_matrix(query)
-    elif ch == "2":
-        inverted_index(query)
-    elif ch == "3":
-        TF_IDF(query)
-    else:
-        print("Invalid Selected Algorithm")
+    ex = ''
+    while ex not in ['n', 'N', 'NO', 'no', 'No']:
+        os.system('cls')
+        ch = input("*Select Retrieving Algorithm*\n"
+                   "1. Term Document Matrix\n"
+                   "2. Inverted Index\n"
+                   "3. TF/IDF\n")
+        query = input("\n---------------------\n"
+                      "Enter Query: ")
+        print("---------------------\n")
+        if ch == "1":
+            term_document_matrix(query)
+        elif ch == "2":
+            inverted_index(query)
+        elif ch == "3":
+            TF_IDF(query)
+        else:
+            print("Invalid Selected Algorithm")
+
+        ex = input('Do you want to continue?(Yes|No) ')
