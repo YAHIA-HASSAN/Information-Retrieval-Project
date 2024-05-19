706b45b111cff6bbe9eebf263f01b3803c5913e2
Yahia Hassan <77647892+YAHIA-HASSAN@users.noreply.github.com>
2024-05-19 23:11:18 +0300

    term document matrix

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-# from preprocessing import process
-# import os
-# process(os.path.dirname(__file__) + '\\Dataset\\',
-#         os.path.dirname(__file__) + '\\Preprocessed\\')
+from preprocessing import process
+from term_document_matrix import retrieve_top_document_names
+import os
 
 
+def preprocess():
+    process(os.path.dirname(__file__) + '\\Dataset\\',
+            os.path.dirname(__file__) + '\\Preprocessed\\')
+
+
+# Term document matrix usage:
+def term_document_matrix(query):
+    top_doc_names = retrieve_top_document_names(query)
+
+    for idx, doc_name in enumerate(top_doc_names):
+        print(f"Top Document {idx + 1}: {doc_name}")
+
+
+def inverted_index(query):
+    pass
+
+
+def TF_IDF(query):
+    pass
+
+
+if __name__ == '__main__':
+    # preprocess()
+    ch = input("Select Retrieving Algorithm:\n"
+               "1. Term Document Matrix\n"
+               "2. Inverted Index\n"
+               "3. TF/IDF\n")
+    query = input("\n---------------------\n"
+                  "Enter Query: ")
+    if ch == "1":
+        term_document_matrix(query)
+    elif ch == "2":
+        inverted_index(query)
+    elif ch == "3":
+        TF_IDF(query)
+    else:
+        print("Invalid Selected Algorithm")
---
+++ term_document_matrix.py
@@ -0,0 +1,62 @@
+import os
+from collections import defaultdict
+
+
+# Function to read documents from files in a directory
+def read_documents_from_directory(directory):
+    documents = []
+    filenames = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):  # Assuming text files
+            with open(os.path.join(directory, filename), "r") as file:
+                documents.append(file.read())
+                filenames.append(filename)
+    return documents, filenames
+
+
+# Function to tokenize text
+def tokenize(text):
+    return text.split()
+
+
+# Function to compute term frequency
+def compute_term_frequency(terms, document):
+    term_freq = defaultdict(int)
+    words = tokenize(document)
+    for word in words:
+        if word in terms:
+            term_freq[word] += 1
+    return term_freq
+
+
+# Function to rank documents based on query
+def rank_documents(query, documents):
+    query_terms = tokenize(query)
+    doc_scores = []
+
+    for idx, doc in enumerate(documents):
+        term_freq = compute_term_frequency(query_terms, doc)
+        score = sum(term_freq[term] for term in query_terms)
+        doc_scores.append((score, idx))
+
+    doc_scores.sort(reverse=True, key=lambda x: x[0])
+    top_docs = [idx for score, idx in doc_scores[:10]]
+
+    return top_docs
+
+
+# Specify the directory containing your documents
+directory = os.path.dirname(os.path.realpath(__file__)) + '\\Preprocessed\\'
+
+# Read documents from files in the directory
+docs, filenames = read_documents_from_directory(directory)
+
+
+# Function to process a query and retrieve top 10 document names
+def retrieve_top_document_names(query):
+    top_doc_indices = rank_documents(query, docs)
+    top_doc_names = [filenames[idx] for idx in top_doc_indices]
+    return top_doc_names
+
+
+
